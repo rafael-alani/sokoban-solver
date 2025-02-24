d113b49815431ba9192740acaa37aaa947276bca
rafael-alani <rafael.alani@hotmail.com>
2025-02-24 20:28:18 +0100

    remove errors

--- src/MyAgent.java
+++ src/MyAgent.java
@@ -7,6 +7,7 @@ import game.actions.EDirection;
 import game.actions.compact.*;
 import game.board.compact.BoardCompact;
 
+import search.*;
 /**
  * The simplest Tree-DFS agent.
  * @author Jimmy
@@ -14,42 +15,43 @@ import game.board.compact.BoardCompact;
 public class MyAgent extends ArtificialAgent {
 	protected BoardCompact board;
 	protected int searchedNodes;
-	
+
 	@Override
 	protected List<EDirection> think(BoardCompact board) {
 		this.board = board;
 		searchedNodes = 0;
 		long searchStartMillis = System.currentTimeMillis();
-		
+
 		List<EDirection> result = new ArrayList<EDirection>();
 		dfs(5, result); // the number marks how deep we will search (the longest plan we will consider)
 
 		long searchTime = System.currentTimeMillis() - searchStartMillis;
-        
-        if (verbose) {
-            out.println("Nodes visited: " + searchedNodes);
-            out.printf("Performance: %.1f nodes/sec\n",
-                        ((double)searchedNodes / (double)searchTime * 1000));
-        }
-		
+
+		if (verbose) {
+			out.println("Nodes visited: " + searchedNodes);
+			out.printf("Performance: %.1f nodes/sec\n",
+					((double)searchedNodes / (double)searchTime * 1000));
+		}
+
 		return result.isEmpty() ? null : result;
 	}
 
 
-public class SokobanProblem <BoardCompact,CAction> implements HeuristicProblem<BoardCompact, CAction>{
+	public class SokobanProblem <BoardCompact,CAction> implements HeuristicProblem<BoardCompact, CAction>{
 
-	@Override
-	public double estimate(BoardCompact state) {
-		// We can start with mannhatan distance estimation
-	}
+		@Override
+		public double estimate(BoardCompact state) {
+			// We can start with mannhatan distance estimation
+		return 0.0;
+		}
 
-	@Override
-	public BoardCompact initialState() {
-		return null;
-	}
+		@Override
+		public BoardCompact initialState() {
+			return null;
+		}
 
-	@Override
-	public List<CAction> actions(BoardCompact state) {
+		@Override
+		public List<CAction> actions(BoardCompact state) {
 //		List<CAction> actions = new ArrayList<CAction>(4);
 //		for (CMove move : CMove.getActions()) {
 //			if (move.isPossible(board)) {
@@ -62,36 +64,36 @@ public class SokobanProblem <BoardCompact,CAction> implements HeuristicProblem<B
 //			}
 //		}
 
-		return List.of();
-	}
+			return List.of();
+		}
 
-	@Override
-	public BoardCompact result(BoardCompact state, CAction action) {
-		return null;
-	}
+		@Override
+		public BoardCompact result(BoardCompact state, CAction action) {
+			return null;
+		}
 
-	@Override
-	public boolean isGoal(BoardCompact state) {
-		return false;
-	}
+		@Override
+		public boolean isGoal(BoardCompact state) {
+			return false;
+		}
 
-	@Override
-	public double cost(BoardCompact state, CAction action) {
-		return 0;
+		@Override
+		public double cost(BoardCompact state, CAction action) {
+			return 0;
+		}
 	}
-}
 
 
 
 	private boolean dfs(int level, List<EDirection> result) {
 		if (level <= 0) return false; // DEPTH-LIMITED
-		
+
 		++searchedNodes;
-		
+
 		// COLLECT POSSIBLE ACTIONS
-		
+
 		List<CAction> actions = new ArrayList<CAction>(4);
-		
+
 		for (CMove move : CMove.getActions()) {
 			if (move.isPossible(board)) {
 				actions.add(move);
@@ -102,30 +104,30 @@ public class SokobanProblem <BoardCompact,CAction> implements HeuristicProblem<B
 				actions.add(push);
 			}
 		}
-		
+
 		// TRY ACTIONS
 		for (CAction action : actions) {
 			// PERFORM THE ACTION
 			result.add(action.getDirection());
 			action.perform(board);
-			
+
 			// CHECK VICTORY
 			if (board.isVictory()) {
 				// SOLUTION FOUND!
 				return true;
 			}
-			
+
 			// CONTINUE THE SEARCH
 			if (dfs(level - 1, result)) {
 				// SOLUTION FOUND!
 				return true;
 			}
-			
+
 			// REVERSE ACTION
 			result.remove(result.size()-1);
 			action.reverse(board);
 		}
-		
+
 		return false;
 	}
 }
